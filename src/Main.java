2fa3f51865e76a773258f9b4d48efdbd72c3afa7
sheha_mgcb38b <shehabkh249@gmail.com>
2024-10-20 17:53:42 +0200

    Added testing statements for adder class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+
+        Adder adder = new Adder();
+        System.out.println(adder.add(1,2));
     }
 }
